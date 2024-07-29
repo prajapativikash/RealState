9a3685aa91c32b9e52ea787c3aace1609932aa3b
vikash prajapati <vikashprajapati6742@gmail.com>
2024-07-30 01:29:30 +0530

    create Header Section and page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignOut from './pages/SignOut'
+import Profile from './pages/Profile'
+import Header from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-out' element={<SignOut />} />
+          <Route path='/Profile' element={<Profile />} />
+
+
+        </Routes>
+      </BrowserRouter>
+
       <h1 className='text-red-600'>App</h1>
     </>
   )
---
+++ src/pages/SignOut.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SignOut() {
+   return (
+      <>
+         signout
+      </>
+   )
+}
+
+export default SignOut
