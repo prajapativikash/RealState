9572ce890c0971488c2252b0eb29826f8ba32e82
vikash prajapati <vikashprajapati6742@gmail.com>
2024-08-03 18:33:12 +0530

    create and run the server

--- Api/index.js
+++ Api/index.js
@@ -1,37 +1,40 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const userRouter = require('./routes/user.router')
-const signupRouter = require('./routes/auth.router.js')
-const cors = require('cors');
+import express from 'express'
+// import mongoose from 'mongoose';
+// const userRouter = require('./routes/user.router')
+// const signupRouter = require('./routes/auth.router.js')
+// const cors = require('cors');
+// import dotenv from 'dotenv'
+// dotenv.config();
+
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/RealState').then(() => {
-   console.log("coonenct database")
-})
+// mongoose.connect(process.env.MONGO).then(() => {
+//    console.log("coonenct database")
+// })
 
-app.use(cors())
-
-
-app.use(express.json())
+// app.use(cors())
 
 
+// app.use(express.json())
 
-app.use('/Api/routes/user', userRouter)
 
-app.use('/Api/routes/auth', signupRouter);
 
-app.use((err, req, res, next) => {
-   const statusCode = err.statusCode || 500;
-   const message = err.message || 'Internal Server Error ';
-   return res.status(statusCode).json({
-      success: false,
-      statusCode,
-      message
+// app.use('/Api/routes/user', userRouter)
 
-   });
+// app.use('/Api/routes/auth', signupRouter);
 
-});
+// app.use((err, req, res, next) => {
+//    const statusCode = err.statusCode || 500;
+//    const message = err.message || 'Internal Server Error ';
+//    return res.status(statusCode).json({
+//       success: false,
+//       statusCode,
+//       message
+
+//    });
+
+// });
 
 
 app.listen(4000, () => {
